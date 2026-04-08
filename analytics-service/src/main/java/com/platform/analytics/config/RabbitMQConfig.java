0b6a90cfa727c1ee98cd8e0d906597df58eca530
computer <stefan.nik04@gmail.com>
2026-04-08 18:11:27 +0200

    fix message converter in analytics consumer

--- analytics-service/src/main/java/com/platform/analytics/config/RabbitMQConfig.java
+++ analytics-service/src/main/java/com/platform/analytics/config/RabbitMQConfig.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,10 +27,5 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue analyticsQueue, TopicExchange analyticsExchange) {
         return BindingBuilder.bind(analyticsQueue).to(analyticsExchange).with("event.#");
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 }
